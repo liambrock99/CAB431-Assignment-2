996e4ca08dcbc62cdbe5283a0fa1c4f5ffe0b67d
liam <liam.brock99@gmail.com>
2020-05-04 16:26:19 +1000

    tfidf ranking

--- BowDoc.py
+++ BowDoc.py
@@ -1,35 +1,55 @@
+from math import log
+
 class BowDoc:
 
-    def __init__(self, doc_id, terms):
+    def __init__(self, doc_id):
         self.doc_id = doc_id
-        self.bow = {}
-        for term in terms:
-            self.add_term(term)
+        self.terms = {}
+
+    def calc_tf(self, term):
+        f = self.terms[term] if term in self.terms else 0
+        return log(1 + f)
 
     def get_doc_id(self):
         return self.doc_id
 
-    def get_bow(self):
-        return self.bow
+    def get_terms(self):
+        return self.terms
 
     def add_term(self, term):
         try:
-            self.bow[term] += 1
+            self.terms[term] += 1
         except KeyError:
-            self.bow[term] = 1
+            self.terms[term] = 1
 
 class BowDocCollection:
     
-    def __init__(self, id):
-        self.bow_col = {}
-        self.id = id
+    def __init__(self, col_id):
+        self.bowdocs = {}
+        self.col_id = col_id
 
-    def get_bowdoc_collection(self):
-        return self.bow_col
+    def get_bowdocs(self):
+        return self.bowdocs
+
+    def calc_idf(self, term):
+        n = len(self.bowdocs)
+        df = 1 # avoid division-by-zero
+        for bowdoc in self.bowdocs.values():
+            if term in bowdoc.get_terms():
+                df += 1
+        return log(n/df) 
 
     def add_bowdoc(self, bowdoc):
-        doc_id = bowdoc.get_doc_id()
-        self.bow_col[doc_id] = bowdoc
+        self.bowdocs[bowdoc.get_doc_id()] = bowdoc
 
+    # Expects a preprocessed tokenized query
     def query(self, query):
-        pass
+        results = {}
+        for docid, bowdoc in self.bowdocs.items():
+            tfidf = 0.0
+            for term in query:
+                idf = self.calc_idf(term)
+                tf = bowdoc.calc_tf(term)
+                tfidf += (tf * idf)
+            results[docid] = tfidf
+        return results
--- TrainingSetDiscovery.py
+++ TrainingSetDiscovery.py
@@ -1,59 +1,63 @@
+import re
+import string
 import xml.etree.ElementTree as ET
+from nltk.corpus import stopwords
 from stemming.porter2 import stem
 from pathlib import Path
 from BowDoc import *
-import re
-import string
 
 class Processor:
     
-    def __init__(self, stop_words, stemmer):
-        self.stop_words = stop_words
-        self.stemmer = stemmer
-
-    def preprocess(self, text):
-        """ 
-        The text is stripped of numbers and punctuation.
-        Then the text is tokenized, stop words are removed and remaining terms stemmed with the Porter2 stemming algorithm.
-        Returns a list of processed terms.    
-        """
+    def preprocess(text):
+        """ Preprocesses text """
         text = text.lower() 
         text = re.sub(r'\d+', '', text) 
         text = text.translate(str.maketrans('',  '', string.punctuation))
         text = text.split()
-        return [self.stemmer(term) for term in text if term not in self.stop_words and len(term) > 3]
+        return [stem(term) for term in text if term not in stopwords.words('english') and len(term) > 3]
 
-    def bowdocify(self, xml_file):
+    def bowdocify(xml_file):
         """ Converts an XML file to a BowDoc """
         root = ET.parse(xml_file).getroot()
         doc_id = root.get('itemid')
         text_el = root.find('text')
-        terms = self.preprocess(' '.join([child.text for child in text_el]))
-        bowdoc = BowDoc(doc_id, terms)
+        terms = Processor.preprocess(' '.join([child.text for child in text_el]))
+        bowdoc = BowDoc(doc_id)
+        for term in terms:
+            bowdoc.add_term(term)
         return bowdoc
 
 if __name__ == "__main__":
-    stop_words = open(Path('stopwords.txt')).read().split()
     dataset = Path('dataset101-150')
     topicset = Path('TopicStatements101-150.txt')
-    p = Processor(stop_words, stem)
+    pra = 1.5 # pseudo relevance assumption
 
-    topics = {}
-    with open(topicset) as file:
-        topic_nums = []
-        titles = []
-        for line in file:
-            if line.startswith('<num>'):
-                topic_nums.append((line.strip()[-3:]))
-            if line.startswith('<title>'):
-                titles.append((line[7:].strip()))
-        topics = dict(zip(topic_nums, titles))
+    bowdocs = BowDocCollection(101)
+    for xml_file in (Path('dataset101-150') / 'Training101').iterdir():
+        bowdocs.add_bowdoc(Processor.bowdocify(xml_file))
+
+    results = bowdocs.query([stem('economic'), stem('espionage')])
+    with open('test.txt', 'w') as file:
+        for k, v in results.items():
+            file.write(f'{k}:{v}\n')
+
+
+    # topics = {}
+    # with open(topicset) as file:
+    #     topic_nums = []
+    #     titles = []
+    #     for line in file:
+    #         if line.startswith('<num>'):
+    #             topic_nums.append((line.strip()[-3:]))
+    #         if line.startswith('<title>'):
+    #             titles.append((line[7:].strip()))
+    #     topics = dict(zip(topic_nums, titles))
     
-    bowdoc_collections = {} # collection of BowDocCollection
-    for topic in topics.keys():
-        bowdoc_col = BowDocCollection(topic)
-        dir = dataset / ('Training' + topic)
-        for xml_file in dir.iterdir():
-            bowdoc_col.add_bowdoc(p.bowdocify(xml_file))
-        bowdoc_collections[topic] = bowdoc_col
+    # bowdoc_collections = {} # collection of BowDocCollection
+    # for topic in topics.keys():
+    #     bowdoc_col = BowDocCollection(topic)
+    #     dir = dataset / ('Training' + topic)
+    #     for xml_file in dir.iterdir():
+    #         bowdoc_col.add_bowdoc(p.bowdocify(xml_file))
+    #     bowdoc_collections[topic] = bowdoc_col
    
